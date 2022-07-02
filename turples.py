ae6b968f2c33096b49f7c90cfb5d44fc76eaa619
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2022-07-02 22:48:54 +0300

    Add files via upload

---
+++ turples.py
@@ -0,0 +1,16 @@
+from turtle import position
+
+
+fruit_turple=("apple","banana","strawberry","orange")
+print(fruit_turple[1])
+print(fruit_turple.index("apple"))
+
+countries=("Kenya","Uganda","India","China","Sudan")
+print(countries)
+num=int(input('enter any number between 0 and 4'))
+print(countries[num])
+
+
+
+
+
